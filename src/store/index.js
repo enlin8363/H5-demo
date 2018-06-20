c14e7ec1623df47b77a50b85995aa81e2092bff8
linnaien <enlin8363@163.com>
2018-06-20 10:43:04 +0800

    git-demo

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+Vue.use(Vuex)
+const state = {
+  count: 0
+}
+const store = new Vuex.Store({
+  state,
+  getters: { // 相当于对 state 数据的过滤
+    filCount (state) {
+      return state.count + 2
+    }
+  },
+  mutations: {
+    increstCount (state) {
+      state.count++
+    }
+  },
+  actions: {
+    increstCount (context) {
+      context.commit('increstCount')
+    }
+  }
+})
+export default store
